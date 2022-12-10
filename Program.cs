c072938efd28dc92ab52d5b49636c46b58ea50bc
Lutin <tclutin01@gmail.com>
2022-12-11 00:13:41 +0500

    Completed the task

--- Program.cs
+++ Program.cs
@@ -4,7 +4,50 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string number = Console.ReadLine();
+            char[] arrayOfChar = number.ToCharArray();
+
+            if (IsCorrect(arrayOfChar))
+            {
+                Console.WriteLine(SumOfNumbers(arrayOfChar));
+            }
+        }
+        private static bool IsCorrect(char[] arrayOfChar)
+        {
+
+            if (arrayOfChar[0] == '-' || Char.IsDigit(arrayOfChar[0]))
+            {
+                for (int i = 1; i < arrayOfChar.Length; i++)
+                {
+                    if (!Char.IsDigit(arrayOfChar[i]))
+                    {
+                        Console.WriteLine("Not correct");
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        private static int SumOfNumbers(char[] arrayOfChar)
+        {
+            int sum = 0;
+            if (arrayOfChar[0] == '-')
+            {
+                for (int i = 1; i < arrayOfChar.Length; i++)
+                {
+                    sum += arrayOfChar[i] - '0';
+                }
+            }
+            else
+            {
+                for (int i = 0; i < arrayOfChar.Length; i++)
+                {
+                    sum += arrayOfChar[i] - '0';
+                }
+            }
+            return sum;
         }
     }
 }
